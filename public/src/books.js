1c02bfc63736dcc5cb60791ea0dc157fda68f1d7
Luis Torres <luisdtor@gmail.com>
2021-01-27 10:49:47 -0600

    Complete qualified assignment, ...

---
+++ public/src/books.js
@@ -0,0 +1,58 @@
+const { getNonReturnedBooks, getReturnedBooks } = require("./helpers");
+const { findAccountById } = require("./accounts");
+
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id);
+}
+
+function findBookById(books, id) {
+  return books.find((book) => book.id === id);
+}
+
+function partitionBooksByBorrowedStatus(books) {
+  // create array of non-returned books
+  const nonReturnedBooks = getNonReturnedBooks(books);
+
+  // create array of returned books
+  const returnedBooks = getReturnedBooks(books);
+
+  // create empty array
+  const result = [];
+
+  // push non-returned books array
+  result.push(nonReturnedBooks);
+
+  // push returned books array
+  result.push(returnedBooks);
+
+  // return array
+  return result;
+}
+
+function getBorrowersForBook(book, accounts) {
+  // create array of transactons from the given book
+  const transactions = book.borrows;
+
+  // use map to add the transaction id's account info to the transaction
+  const result = transactions.map((transaction) => {
+    const accountInfo = findAccountById(accounts, transaction.id);
+    const newTransaction = {
+      ...transaction,
+      ...accountInfo,
+    };
+    return newTransaction;
+  });
+
+  // limit the amount of transactions to 10
+  result.splice(10);
+
+  // return the updated transaciton array
+  return result;
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
---
+++ public/src/helpers.js
@@ -0,0 +1,26 @@
+// A function that takes an array of authors and an author ID and returns an author object.
+const getAuthorById = (authors, id) => {
+  return authors.find((author) => author.id === id);
+};
+
+// Takes an array of books and returns an array of the non-returned books
+const getNonReturnedBooks = (books) => {
+  return books.filter((book) => book.borrows.some((transaction) => !transaction.returned));
+};
+
+// Takes an array of books and returns an array of the returned books
+const getReturnedBooks = (books) => {
+  return books.filter((book) => book.borrows.every((transaction) => transaction.returned));
+};
+
+// Takes an array of books and an author ID and returns an array of books written by the given author
+const getBooksByAuthorId = (books, authorId) => {
+  return books.filter((book) => book.authorId === authorId);
+};
+
+module.exports = {
+  getAuthorById,
+  getNonReturnedBooks,
+  getReturnedBooks,
+  getBooksByAuthorId
+};
